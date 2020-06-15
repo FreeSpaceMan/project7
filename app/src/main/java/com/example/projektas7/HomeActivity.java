cd524dca54839c9d4e82b012d2de39022d76cd57
FreeSpaceMan <sar.bartkus@gmail.com>
2020-06-15 23:10:08 +0100

    Added MapBox mapView to an Activity. Map works now.

--- app/src/main/java/com/example/projektas7/HomeActivity.java
+++ app/src/main/java/com/example/projektas7/HomeActivity.java
@@ -1,8 +1,9 @@
 package com.example.projektas7;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import com.example.projektas7.fragments.FirstFragment;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
@@ -14,22 +15,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
+import android.widget.Button;
 
 
 public class HomeActivity extends AppCompatActivity {
+    private Button btnGoToMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        FirstFragment firstFragment = new FirstFragment();
-        FragmentManager manager = getSupportFragmentManager();
-
-        manager.beginTransaction().add(R.id.homeActivityLayout,firstFragment).commit();
-
-
-
+        btnGoToMap = (Button)findViewById(R.id.button_goToMap);
+        btnGoToMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMapActivity();
+            }
+        });
     }
+
+    public void openMapActivity() {
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
+    }
+
 }
