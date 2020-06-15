41368d2bafdcd47baa4b4312a297f25fbaef0f1e
FreeSpaceMan <sar.bartkus@gmail.com>
2020-06-15 02:28:40 +0100

    Connected HomeActivity to a FirstFragment

--- app/src/main/java/com/example/projektas7/HomeActivity.java
+++ app/src/main/java/com/example/projektas7/HomeActivity.java
@@ -1,19 +1,33 @@
 package com.example.projektas7;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
+import com.example.projektas7.fragments.FirstFragment;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
 
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        FirstFragment firstFragment = new FirstFragment();
+        FragmentManager manager = getSupportFragmentManager();
+
+        manager.beginTransaction().add(R.id.homeActivityLayout,firstFragment).commit();
 
     }
-
-
 }
